6bdd3dc1f1034a44b40de2e801dcf0845667a98c
Parishkar Nagrikar <p4parish@gmail.com>
2023-12-21 15:23:39 +0530

    Prutha : Fixed issues with application yml syntax

---
+++ readme.md
@@ -0,0 +1,6 @@
+This is just a test project in order to check if all the required necessary infra is working on a machine
+
+Requisites
+
+1. Java
+2. IntelliJ
--- src/main/java/com/shopoedia/test/service/TestService.java
+++ src/main/java/com/shopoedia/test/service/TestService.java
@@ -1,9 +1,6 @@
 package com.shopoedia.test.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.shopoedia.test.repository.TestRepository;
 
 @Service
 public class TestService {
